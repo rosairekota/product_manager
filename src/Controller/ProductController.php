0aa30f943aad67d1eae1839083aa891a0701999a
rkota <rosairekota@gmail.com>
2021-12-11 16:38:45 +0100

    change product route by products in productController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/product')]
+#[Route('/products')]
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'product_index', methods: ['GET'])]
