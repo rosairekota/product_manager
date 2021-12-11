43b44bf640970e375d93fdf469c965689f9279b5
rkota <rosairekota@gmail.com>
2021-12-11 16:35:52 +0100

    feat: add migrate and crud

---
+++ src/Form/ProductType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',null,['label'=>'Inserer le nom du produit'])
+            ->add('price',IntegerType::class,['label'=>'Inserer le prix du produit'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
